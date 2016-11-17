a8280a4d71130b073483a1c19a9ed6df7ffd3bcb
David Mendez Guardado <demg@outlook.com>
2016-11-16 23:55:24 -0600

    -Terminada clase "Flor". -eliminados algunos de los errores por falta de codigo, aun falta codigo en la clase "Grafico",

--- src/algoritmos/geneticos/Flor.java
+++ src/algoritmos/geneticos/Flor.java
@@ -10,5 +10,48 @@ package algoritmos.geneticos;
  * @author demg
  */
 public class Flor {
-    
+
+    private double adaptacion;
+    private int x;
+
+    /**
+     * 0 altura 
+     * 1 color 
+     * 2 color del tallo 
+     * 3 ancho del tallo 
+     * 4 tamaño de la flor
+     * 5 tamaño del centro de la flor
+     */
+    private int[] cromosoma;
+
+    public Flor() {
+        cromosoma = new int[6];
+        for (int i = 0; i < cromosoma.length; i++) {
+            cromosoma[i] = 0;
+        }
+    }
+
+    public double getAdaptacion() {
+        return adaptacion;
+    }
+
+    public void setAdaptacion(double adaptacion) {
+        this.adaptacion = adaptacion;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int[] getCromosoma() {
+        return cromosoma;
+    }
+
+    public void setCromosoma(int[] cromosoma) {
+        this.cromosoma = cromosoma;
+    }
 }
